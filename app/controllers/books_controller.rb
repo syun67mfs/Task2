ba113472d524c9ff2d5f66a5a56a1ef791ca98ec
EC2 Default User <ec2-user@ip-172-31-60-147.ec2.internal>
2020-09-18 11:20:06 +0000

    still in task

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+  def top
+  end
+  def index
+    @books = Book.all.order("id ASC")
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to books_path
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated"
+       redirect_to book_path(params[:id])
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed"
+       redirect_to books_path
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
